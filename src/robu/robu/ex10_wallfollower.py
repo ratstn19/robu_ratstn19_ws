73d80bf00bb35a950678a5f5f77dee629a0a5a22
ratstn19 <ratstn19@htl-kaindorf.at>
2023-11-07 13:14:50 +0100

    Wallfollower 7.11.23

--- src/robu/robu/ex10_wallfollower.py
+++ src/robu/robu/ex10_wallfollower.py
@@ -1,6 +1,6 @@
 import math
 import rclpy
-
+from rclpy.node import Node
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
@@ -13,13 +13,13 @@ from enum import IntEnum
 
 
 ROBOT_DIRECTION_FRONT_INDEX = 0
-ROBOT_DIRECTION_RIGHT_FRONT_INDEX = 45
-ROBOT_DIRECTION_RIGHT_INDEX = 90
-ROBOT_DIRECTION_RIGHT_REAR_INDEX = 135
+ROBOT_DIRECTION_RIGHT_FRONT_INDEX = 315
+ROBOT_DIRECTION_RIGHT_INDEX = 270
+ROBOT_DIRECTION_RIGHT_REAR_INDEX = 225
 ROBOT_DIRECTION_REAR_INDEX = 180
-ROBOT_DIRECTION_LEFT_REAR_INDEX = 225
-ROBOT_DIRECTION_LEFT_INDEX = 270
-ROBOT_DIRECTION_LEFT_FRONT_INDEX = 315
+ROBOT_DIRECTION_LEFT_REAR_INDEX = 135
+ROBOT_DIRECTION_LEFT_INDEX = 90
+ROBOT_DIRECTION_LEFT_FRONT_INDEX = 45
 
 class WallFollowerStates(IntEnum):
     WF_STATE_DETECTWALL = 0,
@@ -28,7 +28,7 @@ class WallFollowerStates(IntEnum):
     WF_STATE_FOLLOWWALL = 3
 
 
-class WallFollowerer(rclpy.Node):
+class WallFollower(Node):
     def __init__(self):
         super().__init__('WallFollower')
         self.scan_subscriber = self.create_subscription(LaserScan,"/scan",self.scan_callback, qos_profile_sensor_data)
@@ -40,8 +40,9 @@ class WallFollowerer(rclpy.Node):
         self.rightfront_dist = 999999.9
         self.right_dist = 999999.9
         self.rear_dist = 999999.9
+        self.distances = []
 
-        self.wallfollower_state = WallFollowerer.WF_STATE_DETECTWALL
+        self.wallfollower_state = WallFollowerStates.WF_STATE_DETECTWALL
 
         self.forward_speed_wf_slow = 0.05
         self.forward_speed_wf_fast = 0.1
@@ -52,10 +53,35 @@ class WallFollowerer(rclpy.Node):
         self.dist_threash_wf = 0.3
         self.dist_hysteresis_wf = 0.02
 
+        self.dist_laser_offset = 0.03
+
+        self.valid_lidar_data = False
         self.timer = self.create_timer(0.2, self.timer_callback)
     
     def timer_callback(self):
-        pass
+        if self.valid_lidar_data: #Beim erstmaligen Aufruf liegen och keine LiDAR Daten 
+            self.follow_wall()
+
+    def follow_wall(self):
+        msg = Twist()
+        msg.linear.x = 0.0
+        msg.linear.y = 0.0
+        msg.linear.z = 0.0
+
+        msg.angular.x = 0.0
+        msg.angular.y = 0.0
+        msg.angular.z = 0.0
+
+        if self.wallfollower_state == WallFollowerStates.WF_STATE_DETECTWALL:
+            dist_min = min(self.distances)
+            if self.front_dist > (dist_min + self.dist_laser_offset):
+                if abs(self.front_dist - dist_min) < 0.2:
+                    msg.angular.z = self.turning_speed_wf_slow
+                else:
+                    msg.angular.z = self.turnng_speed_wf_fast
+            else:
+                printf("WF_STATE_DRIVE2WALL")
+                self.wallfollower_state = WallFollowerStates.WF_STATE_DRIVE2WALL
 
     def scan_callback(self, msg):
         
@@ -66,16 +92,17 @@ class WallFollowerer(rclpy.Node):
         self.right_dist = msg.ranges[ROBOT_DIRECTION_RIGHT_INDEX]
         self.rear_dist = msg.ranges[ROBOT_DIRECTION_REAR_INDEX]
 
+        self.valid_lidar_data = True
         print("ld: %.2f m" % self.left_dist,
-              "lfd: %.2f m" % self.left_front_dist,
+              "lfd: %.2f m" % self.leftfront_dist,
               "fd: %.2f m" % self.front_dist,
-              "rfd: %.2f m" % self.right_front_dist,
+              "rfd: %.2f m" % self.rightfront_dist,
               "rd: %.2f m" % self.right_dist,
               "rrd: %.2f m" % self.rear_dist)
 
 def main(args=None):
     rclpy.init(args=args)
-    wallfollower = WallFollowerer()
+    wallfollower = WallFollower()
 
     rclpy.spin(wallfollower)
 
